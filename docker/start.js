ba51c15e6e6c8418da64c2d23542f2d8605afccc
Demarius Chrite <chrite.demarius@gmail.com>
2015-06-26 21:03:28 -0400

    Log boot2docker VM location.

--- docker/start.js
+++ docker/start.js
@@ -1,5 +1,13 @@
 var sys = require('sys')
 var exec = require('child_process').exec
+var os = require('os')
+
+
+if (os.platform == 'darwin') {
+    exec('$(boot2docker shellinit 2> /dev/null)', function(error, stdout) {
+        for (arg in arguments) {if (arg) console.log(arg)}
+    })
+}
 
 exec('docker pull marvambass/nginx-ssl-secure', function(error, stdout){
     console.log(stdout)
