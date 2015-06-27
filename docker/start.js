3a26de52495e51e044e0480ee2de077541eecb6c
Demarius Chrite <chrite.demarius@gmail.com>
2015-06-27 15:11:40 -0400

    Don't deal with output.

--- docker/start.js
+++ docker/start.js
@@ -2,13 +2,17 @@ var sys = require('sys')
 var exec = require('child_process').exec
 var os = require('os')
 
+function execOut (error, stdout, stderr) {
+    for (arg in arguments) {if (arg) console.log(arg)}
+}
 
 if (os.platform == 'darwin') {
     exec('$(boot2docker shellinit 2> /dev/null)', function(error, stdout) {
-        for (arg in arguments) {if (arg) console.log(arg)}
+        execOut(arguments)
+        if (error != null) {
+            exec('boot2docker up', execOut)
+        }
     })
 }
 
-exec('docker pull marvambass/nginx-ssl-secure', function(error, stdout){
-    console.log(stdout)
-});
+exec('docker pull marvambass/nginx-ssl-secure', execOut)
