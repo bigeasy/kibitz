6bfdaa16843fe4d2fa64eae353952a56ac7c9d16
Demarius Chrite <chrite.demarius@gmail.com>
2015-08-07 16:52:07 -0400

    Bad ideas.

--- docker/start.js
+++ docker/start.js
@@ -1,6 +1,7 @@
 var sys = require('sys')
 var exec = require('child_process').exec
 var os = require('os')
+var fs = require('fs')
 
 function execOut (error, stdout, stderr) {
     for (arg in arguments) {if (arg != null) console.log(arg)}
@@ -18,7 +19,7 @@ if (os.platform == 'darwin') {
 }
 
 exec('docker pull ubuntu', execOut)
-exec('mkdir ~/kibitz', execOut)
-exec('git clone git@github.com:bigeasy/kibitz.git ~/kibitz')
+exec('mkdir ./kibitz', execOut)
+exec('git clone git@github.com:bigeasy/kibitz.git ./kibitz')
 exec('docker build -t kibitz .')
-exec('docker run -v ~/kibitz ' + os.hostname() + '/kibitz')
+exec('docker run -v ./kibitz ' + os.hostname() + '/kibitz')
