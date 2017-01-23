37dc7d592895b6ec3eecdfee37a75b5a3591a54f
Alan Gutierrez <alan@prettyrobots.com>
2017-01-22 21:19:02 -0600

    Rewrite to use procession.

--- t/kibitzer.t.js
+++ t/kibitzer.t.js
@@ -1,96 +1,70 @@
-require('proof/redux')(7, require('cadence')(prove))
+require('proof/redux')(2, require('cadence')(prove))
 
 function prove (async, assert) {
     var cadence = require('cadence')
-    var delta = require('delta')
+    var Procession = require('procession')
 
     var Kibitzer = require('..')
+    var kibitzers = []
 
-    function copy (object) {
-        return JSON.parse(JSON.stringify(object))
-    }
-
-    new Kibitzer({ kibitzerId: '1', ua: {} }).shutdown()
-
-    var kibitzer = new Kibitzer({ kibitzerId: '1', timeout: 1001, ua: {} })
-    assert(kibitzer.legislator.timeout, 1001, 'numeric timeout')
-    kibitzer.shutdown()
-
-    var port = 8086, identifier = 0
-    function createIdentifier () { return String(++identifier) }
-    function createLocation () { return '127.0.0.1:' + (port++) }
-
-    var kibitzers = [], balancerIndex = 0
-    var ua = {
-        send: cadence(function (async, properties, post) {
-            async(function () {
-                if (properties == 'bogus') {
-                    throw new Error
-                }
-                kibitzers.filter(function (kibitzer) {
-                    return kibitzer.properties.location == properties.location
-                }).pop().dispatch(copy(post), async())
-            }, function (result) {
-                return [ copy(result) ]
-            })
-        })
-    }
-
-    var time = 0, options = {
-        notify: function () {},
-        syncLength: 24,
-        ua: ua,
-        __Date: { now: function () { return time } }
-    }
-    async(function () {
-        kibitzers.push(new Kibitzer(extend({
-            kibitzerId: createIdentifier(),
-            properties: { location: createLocation() }
-        }, options)))
-    }, function () {
-        kibitzers[0].bootstrap(0)
-    }, function () {
-        assert(kibitzers[0].legislator.properties[1].location, '127.0.0.1:8086', 'bootstraped')
-        kibitzers.push(new Kibitzer(extend({
-            kibitzerId: createIdentifier(),
-            properties: { location: '127.0.0.1:8088' }
-        }, options)))
-        kibitzers[1].join({ location: '127.0.0.1:8086', islandId: 0 }, async())
-        delta(async()).ee(kibitzers[1]).on('enqueued')
-    }, function () {
-        assert(kibitzers[1].shift().type, 'join', 'joined')
-        assert(kibitzers[1].shift().entry.promise, '2/0', 'immigrated')
-        var cookie = kibitzers[1].publish({ count: 1 })
-        var loop = async(function () {
-            async(function () {
-                delta(async()).ee(kibitzers[1]).on('enqueued')
-            }, function () {
-                var message = kibitzers[1].shift()
-                if (message != null) {
-                    return [ loop.break, message ]
-                }
-            })
-        })()
-    }, function (message) {
-        assert(message.entry.value, { count: 1 }, 'publish')
-    }, function () {
-        kibitzers[1]._enqueue({ entries: [{}] }, async())
-    }, function (response) {
-        assert(response, [], 'failed enqueue')
-        kibitzers[1].shutdown()
-        kibitzers[1].shutdown()
-    }, function () {
-        assert(true, 'terminated')
-        kibitzers[0].shutdown()
-    })
-
-    function extend (to) {
-        var vargs = [].slice.call(arguments, 1)
-        for (var i = 0, I = vargs.length; i < I; i++) {
-            for (var key in vargs[i]) {
-                to[key] = vargs[i][key]
+    function Transform (transformer) {
+        if (transformer.length == 2) {
+            this._transformer = transformer
+        } else {
+            this._transformer = function (value, callback) {
+                transformer(value)
+                callback()
             }
         }
-        return to
+        this.outbox = new Procession
+    }
+
+    Transform.prototype.enqueue = cadence(function (async, value) {
+        async(function () {
+            this._transformer.call(null, value, async())
+        }, function (value) {
+            this.outbox.enqueue(value, async())
+        })
+    })
+
+    kibitzers.push(createKibitzer())
+    assert(kibitzers[0], 'construct')
+    kibitzers[0].bootstrap('x')
+
+    var consumer = kibitzers[0].log.consumer()
+
+    async(function () {
+        kibitzers.push(createKibitzer())
+        kibitzers[1].join({ islandId: 'x', location: '0' }, async())
+    }, function () {
+        kibitzers.push(createKibitzer())
+        kibitzers[2].join({ islandId: 'x', location: '1' }, async())
+    }, function () {
+        consumer.join(function (entry) {
+            return entry.value.value && entry.value.value.value == 1
+        }, async())
+        kibitzers[2].publish({ value: 1 })
+    }, function (entry) {
+        assert(entry.value.value, { value: 1 }, 'published')
+        kibitzers.forEach(function (kibitzer) { kibitzer.shutdown(async()) })
+    }, function () {
+        kibitzers.forEach(function (kibitzer) { kibitzer.shutdown(async()) })
+    })
+
+    function createKibitzer () {
+        var transform = new Transform(cadence(function (async, envelope) {
+            async(function () {
+                var json = JSON.parse(JSON.stringify(envelope.body))
+                kibitzers.filter(function (kibitzer) {
+                    return kibitzer.properties.location == envelope.to.location
+                }).pop().dispatch(json, async())
+            }, function (response) {
+                return { to: envelope.from, body: response }
+            })
+        }))
+        var id = String(kibitzers.length)
+        var kibitzer = new Kibitzer({ id: id, properties: { location: id } })
+        kibitzer.messenger.outbox.pump(transform).outbox.pump(kibitzer.messenger.inbox)
+        return kibitzer
     }
 }
