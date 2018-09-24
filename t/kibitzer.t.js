5bd122c49b126013fa5480e93d9ba21088c2f64e
Alan Gutierrez <alan@prettyrobots.com>
2018-09-24 00:01:11 -0500

    Update unit test structure._

--- t/kibitzer.t.js
+++ t/kibitzer.t.js
@@ -1,6 +1,6 @@
-require('proof')(3, require('cadence')(prove))
+require('proof')(3, prove)
 
-function prove (async, okay) {
+function prove (okay, callback) {
     var cadence = require('cadence')
     var Procession = require('procession')
 
@@ -18,7 +18,7 @@ function prove (async, okay) {
 
     var destructible = new Destructible('t/kibitzer.t.js')
 
-    destructible.completed.wait(async())
+    destructible.completed.wait(callback)
 
     var createKibitzer = cadence(function (async, destructible, id, republic) {
         var kibitzer = new Kibitzer({
@@ -39,49 +39,49 @@ function prove (async, okay) {
         })
     })
 
-    async([function () {
-        console.log('destructed')
-    }], function () {
-        destructible.monitor([ 'kibitzer', 0 ], true, createKibitzer, '0', 0, async())
-    }, function (kibitzer) {
-        kibitzers.push(kibitzer)
-        okay(kibitzers[0], 'construct')
-        kibitzers[0].bootstrap(1, { location: '0' })
+    cadence(function (async) {
+        async(function () {
+            destructible.monitor([ 'kibitzer', 0 ], true, createKibitzer, '0', 0, async())
+        }, function (kibitzer) {
+            kibitzers.push(kibitzer)
+            okay(kibitzers[0], 'construct')
+            kibitzers[0].bootstrap(1, { location: '0' })
 
-        shifter = kibitzers[0].paxos.log.shifter()
-    }, function () {
-        destructible.monitor([ 'kibitzer', 0 ], true, createKibitzer, '1', 0, async())
-    }, function (kibitzer) {
-        kibitzers.push(kibitzer)
-        kibitzers[1].join(1)
-        kibitzers[0].embark(1, '1', kibitzers[1].paxos.cookie, { location: '1' })
-    }, function () {
-        setTimeout(async(), 100)
-    }, function () {
-        destructible.monitor([ 'kibitzer', 0 ], true, createKibitzer, '2', 0, async())
-    }, function (kibitzer) {
-        kibitzers.push(kibitzer)
-        kibitzers[2].join(1)
-        kibitzers[0].embark(1, '2', kibitzers[2].paxos.cookie, { location: '2' })
-        kibitzers[2].paxos.log.shifter().join(function (entry) {
-            return entry.promise == '3/0'
-        }, async())
-    }, function () {
-        kibitzers[2].acclimate()
-        kibitzers[2].publish(1)
-        kibitzers[0].publish(1)
-    }, function () {
-        shifter.join(function (entry) {
-            return entry.promise == '3/1'
-        }, async())
-    }, function (entry) {
-        okay(entry.body.body, 1, 'published')
-        kibitzers[2].request({
-            method: 'enqueue',
-            body: { cookie: '1', republic: 0, entries: [ '1' ] }
-        }, async())
-    }, function (response) {
-        okay(response, null, 'failed submission')
-        destructible.destroy()
-    })
+            shifter = kibitzers[0].paxos.log.shifter()
+        }, function () {
+            destructible.monitor([ 'kibitzer', 0 ], true, createKibitzer, '1', 0, async())
+        }, function (kibitzer) {
+            kibitzers.push(kibitzer)
+            kibitzers[1].join(1)
+            kibitzers[0].embark(1, '1', kibitzers[1].paxos.cookie, { location: '1' })
+        }, function () {
+            setTimeout(async(), 100)
+        }, function () {
+            destructible.monitor([ 'kibitzer', 0 ], true, createKibitzer, '2', 0, async())
+        }, function (kibitzer) {
+            kibitzers.push(kibitzer)
+            kibitzers[2].join(1)
+            kibitzers[0].embark(1, '2', kibitzers[2].paxos.cookie, { location: '2' })
+            kibitzers[2].paxos.log.shifter().join(function (entry) {
+                return entry.promise == '3/0'
+            }, async())
+        }, function () {
+            kibitzers[2].acclimate()
+            kibitzers[2].publish(1)
+            kibitzers[0].publish(1)
+        }, function () {
+            shifter.join(function (entry) {
+                return entry.promise == '3/1'
+            }, async())
+        }, function (entry) {
+            okay(entry.body.body, 1, 'published')
+            kibitzers[2].request({
+                method: 'enqueue',
+                body: { cookie: '1', republic: 0, entries: [ '1' ] }
+            }, async())
+        }, function (response) {
+            okay(response, null, 'failed submission')
+            destructible.destroy()
+        })
+    })(destructible.monitor('test'))
 }
